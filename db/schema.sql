f7ca2ce9b3d0834507a7408bbb1eca5b8676377c
JRM <johnrhmartin@gmail.com>
2022-04-03 16:40:30 -0400

    added roles schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,14 @@
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    CONSTRAINT department_name FOREIGN KEY (role_id) REFERENCES department(id) ON DELETE SET NULL
+)
